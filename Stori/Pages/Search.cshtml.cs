8827ec85c5b325396fc49c1b05a4a8dc677de009
Shannon Kao <shakao@microsoft.com>
2018-07-24 23:57:36 -0700

    Login page css, search javascript

--- Stori/Pages/Search.cshtml.cs
+++ Stori/Pages/Search.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stori.ObjectModel;
 
 namespace Stori.Pages
 {
     public class SearchModel : PageModel
     {
+        private Stori.DataAccessLayer.Dal db = Stori.DataAccessLayer.Dal.Instance;
+
+        public List<Post> Results { get; set; }
+
+        public SearchModel()
+        {
+            Results = new List<Post>();
+        }
+
         public void OnGet()
         {
-
+            this.Results = db.GetPostsByUsername(HttpContext.Request.Query["q"].ToString());
         }
     }
 }
---
+++ Stori/wwwroot/js/search.js
@@ -0,0 +1,16 @@
+(function() {
+  var sb = null;
+
+  function init() {
+    sb = document.getElementById('searchbox');
+
+    sb.addEventListener('keypress', function(evt){
+        if (evt.which == 13 || evt.keyCode == 13) {
+            window.location.replace("/search?q=" + sb.value);
+        }
+    }, false);
+  }
+
+  window.addEventListener('load', init, false);
+})();
+
