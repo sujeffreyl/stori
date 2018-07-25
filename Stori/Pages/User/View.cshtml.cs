5c02fbbd0de3691c22b96daf4269f1c66d7353f3
Jeffrey Su <jefsu@microsoft.com>
2018-07-25 16:24:20 -0700

    Display user profile picture on posts

--- Stori/Pages/User/View.cshtml.cs
+++ Stori/Pages/User/View.cshtml.cs
@@ -14,6 +14,8 @@ namespace Stori.Pages.User
 
         public List<Post> Posts { get; set; }
 
+        public Stori.ObjectModel.User User { get; set; }
+
         public ViewModel()
         {
             Posts = new List<Post>();
@@ -21,13 +23,27 @@ namespace Stori.Pages.User
 
         public void OnGet()
         {
-            //this.Posts = db.GetAllPosts();
             this.Posts = db.GetPostsByUsername(RouteData.Values["username"].ToString());
-            //Post testPost = new Post();
-            //testPost.Title = "Hard-coded Title";
-            //testPost.CreateDate = DateTime.Now;
-            //testPost.Text = "Hard-coded value";
-            //this.Posts.Add(testPost);
+
+            if (this.Posts.Any())
+            {
+                var nonBlankUserProfiles = this.Posts.Where(post => !String.IsNullOrEmpty(post.Author.ProfilePictureUrl));
+                if (nonBlankUserProfiles.Any())
+                {
+                    this.User = nonBlankUserProfiles.First().Author;
+                }
+                else
+                {
+                    var firstPost = this.Posts.First();
+                    this.User = firstPost.Author;
+                }
+
+                if (String.IsNullOrEmpty(this.User.BannerImageUrl))
+                    this.User.BannerImageUrl = "https://images.unsplash.com/photo-1486427944299-d1955d23e34d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=07df4ad552d3627b7cbc7836321d9a03&auto=format&fit=crop&w=1350&q=80";
+
+                if (String.IsNullOrEmpty(this.User.ProfilePictureUrl))
+                    this.User.ProfilePictureUrl = "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3ccc1801fd91dc9cf869fa6a09360c09&auto=format&fit=crop&w=1000&q=80";
+            }
         }
     }
 }
