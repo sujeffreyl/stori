a075739383c69aafc6fbf86d120b830c6eed3e5b
Shannon Kao <shakao@microsoft.com>
2018-07-26 09:13:08 -0700

    Hide entity if query doesn't mach, message for when no results are present

--- Stori/wwwroot/js/site.js
+++ Stori/wwwroot/js/site.js
@@ -1,17 +1,19 @@
 ﻿// Write your Javascript code.
 (function () {
-    document.getElementById('camera_button').addEventListener('click', function (ev) {
-        var doc = window.document;
-        var docEl = doc.documentElement;
+    if (document.getElementById('camera_button')) {
+        document.getElementById('camera_button').addEventListener('click', function (ev) {
+            var doc = window.document;
+            var docEl = doc.documentElement;
 
-        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+            var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+            var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
 
-        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-            requestFullScreen.call(docEl);
-        }
-        else {
-            cancelFullScreen.call(doc);
-        }
-    }, false);
+            if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+                requestFullScreen.call(docEl);
+            }
+            else {
+                cancelFullScreen.call(doc);
+            }
+        }, false);
+    }
 })();
